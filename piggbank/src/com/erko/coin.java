0cebfaca1429606008a71e656b20501c02b83018
Firealem Erko <firealemg@yahoo.com>
2019-07-12 11:22:06 -0400

    abstract method added

--- piggbank/src/com/erko/coin.java
+++ piggbank/src/com/erko/coin.java
@@ -1,1 +1,26 @@
 package com.erko;
+
+public abstract class Coin {
+
+    int count = 1;
+    double value;
+//constructor
+    public Coin(int count) {
+        this.count = count;
+
+    }
+//toal value method
+    public double getValue() {
+        return value * count;
+    }
+
+//abstract method as a blueprint to return the count of each coin type
+    public abstract String coinValue();
+
+//override default tostring 
+    @Override
+    public String toString() {
+        return "Coin{" + "count = " + count + ", value = " + value + '\n';
+
+    }
+}
